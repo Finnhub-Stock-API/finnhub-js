b8c98336ca842ef137a8b40a9bb83de6db2a8a4e
Dat Tran <dattran2346@gmail.com>
2025-09-29 15:51:50 +0700

    update 2.0.11

--- test.js
+++
@@ -1,7 +0,0 @@
-const finnhub = require('finnhub');
-const finnhubClient = new finnhub.DefaultApi("d1f6f3hr01qsg7dao6t0d1f6f3hr01qsg7dao6tg") // Replace this
-
-//Financials
-finnhubClient.financials("AAPL", "ic", "annual", false, (error, data, response) => {
-    console.log(data)
-});
